16cab90fd9021ba0251ec0fadb4c2541c65d3135
shravan20 <mrshravankumarb@gmail.com>
2020-11-01 17:18:25 +0530

    Added todoist-scripting-template

---
+++ index.js
@@ -0,0 +1,7 @@
+/* Select all the necessary Elements  */
+var input = document.querySelector('.todo_input');
+var MainTodoContainer = document.getElementById('todos')
+var addingButton = document.querySelector('.add-item');
+var deleteAllBtn = document.querySelector('.deleteBtn');
+var completedButton = document.querySelector('.completed');
+var removeButton = document.querySelector('.trash');
